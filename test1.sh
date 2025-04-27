a01f7a722ddd1a9ece21e8cc320fd65382e682a9
matheus <matheuscosta.tech@gmail.com>
2025-04-27 15:22:55 -0300

    Bash script to send message

---
+++ test1.sh
@@ -0,0 +1,17 @@
+# Load environment variables from .env file
+export $(grep -v '^#' .env | xargs)
+
+curl -X POST https://graph.facebook.com/v22.0/$PHONE_NUMBER_ID/messages \
+  -H "Authorization: Bearer $AUTH_TOKEN" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "messaging_product": "whatsapp",
+    "to": "'"$PHONE_NUMBER"'",
+    "type": "template",
+    "template": {
+      "name": "hello_world",
+      "language": {
+        "code": "en_US"
+      },
+    }
+  }'
